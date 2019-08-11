2a0f511499c4fb4bfdf843a06258880b4e5ee218
Thomas Mahler <thma32@web.de>
2019-08-11 12:09:28 +0200

    get rid of TH

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Lib
     ( startApp
-    , app
+    , app1
     ) where
 
+import GHC.Generics
 import Data.Aeson
-import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
@@ -16,25 +16,26 @@ data User = User
   { userId        :: Int
   , userFirstName :: String
   , userLastName  :: String
-  } deriving (Eq, Show)
+  } deriving (Eq, Show, Generic)
 
-$(deriveJSON defaultOptions ''User)
+instance ToJSON User
 
-type API = "users" :> Get '[JSON] [User]
-
-startApp :: IO ()
-startApp = run 8080 app
-
-app :: Application
-app = serve api server
-
-api :: Proxy API
-api = Proxy
-
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
+users1 :: [User]
+users1 = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
         ]
+
+type UserAPI1 = "users" :> Get '[JSON] [User]
+
+-- boilerplate to guide type inference
+userAPI1  :: Proxy UserAPI1
+userAPI1  = Proxy
+
+server1 :: Server UserAPI1
+server1 = return users1
+
+app1 :: Application
+app1 = serve userAPI1 server1
+
+startApp :: IO ()
+startApp = run 8080 app1
